7bac5dc7e2398b4b0b3c65a7f5e0f79e3cfc94f6
leezahuor <leezahuor@gmail.com>
2022-12-06 10:19:20 -0800

    edits

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -18,7 +18,7 @@ class TicTacToe:
     @staticmethod
     def print_board_nums():
         # 0 | 1 | 2 etc (tells us what number corresponds to what box)
-        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        number_board = [[str(i) for i in range(j*3,(j+1) * 3)] for j in range(3)]
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
 
